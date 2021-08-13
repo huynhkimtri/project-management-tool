ef53db7177cd735a0122805630fe4ac866607322
Kim-Tri Huynh <huynhkimtri.fuhcm@gmail.com>
2021-08-13 19:33:11 +0700

    Using map validation for project

--- src/main/java/com/spring/sample/ppm/validation/ValidationError.java
+++ src/main/java/com/spring/sample/ppm/validation/ValidationError.java
@@ -1,4 +1,25 @@
 package com.spring.sample.ppm.validation;
 
-public class ValidatitionError {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ValidationError {
+
+	public ResponseEntity<Object> mapValidationService(BindingResult result) {
+		Map<String, String> errors = null;
+		if (result.hasErrors()) {
+			errors = new HashMap<>();
+			for (FieldError err : result.getFieldErrors()) {
+				errors.put(err.getField(), err.getDefaultMessage());
+			}
+		}
+		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+	}
 }
--- src/main/java/com/spring/sample/ppm/web/ProjectController.java
+++ src/main/java/com/spring/sample/ppm/web/ProjectController.java
@@ -2,18 +2,18 @@ package com.spring.sample.ppm.web;
 
 import com.spring.sample.ppm.domain.Project;
 import com.spring.sample.ppm.services.ProjectService;
+import com.spring.sample.ppm.validation.ValidationError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,12 +22,15 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 
+	@Autowired
+	private ValidationError validationError;
+
 	@PostMapping("")
 	public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
-		if (result.hasErrors()) {
-			return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
+		ResponseEntity<?> errors = validationError.mapValidationService(result);
+		if (Objects.nonNull(errors)) {
+			return errors;
 		}
-
 		Project newProject = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<>(newProject, HttpStatus.CREATED);
 	}
